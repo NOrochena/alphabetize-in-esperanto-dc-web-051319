f55cdf9785c64b3488e8a1ba9c6c5c0fbcd3ad90
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-05-06 01:21:12 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,8 +2,10 @@ def alphabetize(arr)
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort do |a,b|
     
-    esperanto_alphabet.split("").each do |val|
-      
+    a.split("").each do |char|
+      esperanto_alphabet.split("").each do |val|
+        
+      end
     end
   end
 end
